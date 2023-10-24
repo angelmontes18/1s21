5f5f0eeea2b6542d8cbbbd86696d74711a355ebf
Jose Angel Montes Heras <144179731+angelmontes18@users.noreply.github.com>
2023-10-24 14:09:00 -0600

    Add files via upload

---
+++ p2/P2p11_1s21.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class P2p11_1s21{
+	public static void main(String[] args) {
+		for( String cadena : args){
+			
+		}
+		if (args.length == 4){
+			char mensaje = args[0].charAt(1);
+			String mensaje1="1";
+			switch(mensaje){
+		    case'a': System.out.println("Mensaje de analisis de datos")
+			case 'b': System.out.println("Mensaje para instalacion avanzada") ;break;
+			case"/h": System.out.println("Mostrara toda esta lista de parametros ") ;break;
+			case"/e": System.out.println("Ejecutara el programa sin ningun elemento") ;break;
+}
+}
+}
+}
