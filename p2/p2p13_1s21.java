7de5e6c15099503f3a9d24fb693a091bcbf259dc
Jose Angel Montes Heras <144179731+angelmontes18@users.noreply.github.com>
2023-10-27 18:59:22 -0600

    Add files via upload

---
+++ p2/p2p13_1s21.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class p2p13_1s21{
+	public static void main(String[] args){
+		Scanner  teclado = new Scanner(System.in);
+		int f,c;
+            System.out.println("ingrese el numero de filas");
+			f = teclado.nextInt();
+			System.out.println("ingrese el numero de columnas");
+			c = teclado.nextInt();
+			int [][] MA = new int[f][c];
+			int [][] MB = new int[f][c];
+			int [][] MC = new int[f][c];
+            int ff,fc;
+			for(ff=0;ff<f;ff++){
+			    for(fc=0;fc<c;fc++){
+                    System.out.println("ingrese el valor para la posicion[ "+ff+" ][ "+fc+" ]");
+                    MA [ff][fc]= teclado.nextInt();
+                    }
+                }
+	            for(ff=0;ff<f;ff++){
+	                for(fc=0;fc<c;fc++){
+	                System.out.println("ingrese el valor para la posicion["+ff+"]["+fc+"]");
+	                MB[ff][fc]=teclado.nextInt();
+	            }
+	        }
+	        for(ff=0;ff<f;ff++){
+	        	for(fc=0;fc<c;fc++){
+	        		MC[ff][fc]=MA[ff][fc]-MB[ff][fc];
+	        		}
+	        	}
+	        	for(ff=0;ff<f;ff++){
+	        	   for(fc=0;fc<c;fc++){
+	        	   System.out.println("el resultado de la suma de la matriz en su posicion ["+ff+"]["+fc+"] es "+MC[ff][fc]);
+	        	}
+	        }
+	    }
+	}
